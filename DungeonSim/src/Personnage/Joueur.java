a20645e3e3edf5e6df0a90c1025aec904a32f810
Adrien <www.adrienhus@gmail.com>
2025-05-21 11:01:26 +0200

    Implémente l'affichage du menu au démarrage

--- DungeonSim/src/Personnage/Joueur.java
+++ DungeonSim/src/Personnage/Joueur.java
@@ -1,11 +1,23 @@
 package Personnage;
 
-import java.util.Objects;
+import StateJoueur.*;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Joueur extends Personnage{
 
+    private StateJoueur etatJoueur;
+
+
+    Joueur(StateJoueur etatJoueur){
+        this.etatJoueur = new Menu();
+    }
+
     @Override
     protected String choisirAction() {
+        etatJoueur.afficheChoix(); //selon l'état du joueur, affiche les commandes du menu, de combat, de déplacement ou d'interaction
+
         return "";
     }
     //@TODO implémenter le choix d'action selon l'état du joueur
--- DungeonSim/src/StateJoueur/Menu.java
+++ DungeonSim/src/StateJoueur/Menu.java
@@ -1,8 +1,32 @@
 package StateJoueur;
 
+import java.util.Scanner;
+
 public class Menu extends StateJoueur{
+
+    public Menu(){
+        afficheChoix();
+    }
+
     @Override
     public void afficheChoix() {
-        //@TODO nouvelle partie, quitter(sort du programme)
+        boolean flagSortieMenue = false;
+
+        System.out.println("Menu");
+        System.out.println("Entrer NV pour commencer une nouvelle partie ou Q pour quitter");
+        Scanner scanner = new Scanner(System.in);
+
+        while(!flagSortieMenue){
+            String action = scanner.nextLine();
+            if(action.equalsIgnoreCase("q")){
+                System.exit(0);//on sort du programme
+            } else if (action.equalsIgnoreCase("nv")){
+                //@TODO génère une carte et y place le joueur
+                flagSortieMenue = true;
+            }else {
+                System.out.println("Action invalide");
+            }
+
+        }
     }
 }
